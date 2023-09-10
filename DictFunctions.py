d854eb60d992eadb85f29b9ff8c7d3cf9553013f
Ankur Saini <102854487+Ankur-Saini@users.noreply.github.com>
2023-09-10 18:50:10 +0530

    Add files via upload

---
+++ DictFunctions.py
@@ -0,0 +1,37 @@
+a={1:2,3:4,"list":[1,23],"dict":{1:2}}
+a
+
+a['list']
+a.get('list')
+print(a.get("li"))
+a.get('li',0)
+a.keys()
+a.values()
+a.items()
+
+for i in a:
+    print(i,a[i])
+
+for i in a.values():
+    print(i)
+
+"list" in a
+
+2 in a
+
+a
+a['t']=(1,2,3)
+a
+a[1]=10
+a
+b={3:5,'the':4,2:100}
+a.update(b)
+a
+a.pop('t')
+a
+del a[1]
+a
+a.clear()
+a
+del a
+a
