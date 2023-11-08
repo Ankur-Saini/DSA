02870866aa109d928d2b4eb7fc135bea1b24b4ea
Ankur Saini <102854487+Ankur-Saini@users.noreply.github.com>
2023-11-08 19:28:23 +0530

    Add files via upload

---
+++ Graphs/Implementation.py
@@ -0,0 +1,29 @@
+class Graph:
+
+    def __init__(self, nVertices):
+        self.nVertices = nVertices
+        self.adjMatrix = [[0 for i in range(self.nVertices)] for i in range(self.nVertices)]
+
+    def addEdge(self,v1, v2):
+        self.adjMatrix[v1][v2] = 1
+        self.adjMatrix[v2][v1] = 1
+
+    def removeEdge(self, v1, v2):
+        if self.containsEdge(v1, v2):
+            self.adjMatrix[v1][v2] = 0
+            self.adjMatrix[v2][v1] = 0
+
+    def containsEdge(self, v1, v2):
+        return self.adjMatrix[v1][v2] == 1
+    
+    def __str__(self):
+        return str(self.adjMatrix)
+    
+graph1 = Graph(5)
+graph1.addEdge(0,1)
+graph1.addEdge(2,3)
+graph1.addEdge(4,4)
+print(graph1)
+print(graph1.containsEdge(1,0))
+graph1.removeEdge(2,3)
+print(graph1)
